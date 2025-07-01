294a56f29cfc77163094dfecde3265b40bbf433f
최은희 <polarisinuniverse@choeeunhuiui-iMac.local>
2025-07-01 19:08:56 +0900

    Feat: 추가 몬스터 클래스 > attack 함수> 결과 print문 추가

--- lib/game.dart
+++ lib/game.dart
@@ -1,16 +1,14 @@
 import 'dart:io';
 import 'unit.dart';
+import 'settings.dart';
 
 class Game {
-String character = Character('',0,0,0);
-List<Monster>;
-int monsterNum; 
-
+// unit.dart에서 지정한 character 객체 생성
+Character character = Character();
+List <Monster>[]; // 몬스터 리스트
+int monsterNum = 0; // 물리친 몬스터 개수, 몬스터 리스트 개수보다 클 수 없음
 
 void startGame(){
-  if(Character.hp <= 0){
-
-  }
 
 }
 
